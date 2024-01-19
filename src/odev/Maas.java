38dfe80b3ba284a6426bacb33ef7c171ab9bd050
Cansu <cansu.sahin@thy.com>
2024-01-19 10:07:06 +0300

    odevin commiti

--- src/odev/Maas.java
+++ src/odev/Maas.java
@@ -5,7 +5,6 @@ public class Maas {
     // günlük ücret ödevde belirtilmemiş, kendimce 200 tl olarak hesaplama yaptım
 
     public int maasHesapla(int gun){
-        int prim=0;
         int maas;
         if(gun>25){
             maas = (gun*200)+(gun-25)*1000;
